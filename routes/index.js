5b2b2ddcba1b223b44b5640ff9858432e623f957
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-07-05 22:16:33 +0800

    add - MESSAGE

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure App Server' });
+  const data = { title: 'Azure App Server' , message : process.env.MESSAGE || 'this is development'}
+  res.render('index', data);
 });
 
 module.exports = router;
